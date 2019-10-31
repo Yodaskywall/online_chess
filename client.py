257c5165613a0f5a85cb12b25d5b0fdf7c0a165f
Daniel Berenguer <danielbmasia@gmail.com>
2019-10-31 12:41:33 +0100

    Tutorial 1 Complete

---
+++ client.py
@@ -0,0 +1,66 @@
+import pygame
+
+WIDTH = 500
+HEIGHT = 500
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+
+class Player:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x, y, width, height)
+        self.vel = 3
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
+
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def redraw_window(win, player):
+    win.fill((255, 255, 255))
+    player.draw(win)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    p = Player(50, 50, 100, 100, (0, 255, 0))
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        p.move()
+        redraw_window(win, p)
+
+
+if __name__ == "__main__":
+    main()
